ce84d400d079dc43c76c2f800d2d6a7a418c4064
Derick Salamanca <derickzsalamanca@gmail.com>
2020-08-27 18:19:34 -0700

    asdfaeqgq3rgq

---
+++ githubtest/src/githubtest11/Sdvx.java
@@ -0,0 +1,9 @@
+package githubtest11;
+
+public class Sdvx {
+	int i = 0;
+	
+	public void test1() {
+		System.out.println("123131332");
+	}
+}
--- githubtest/src/githubtest11/SoundVoltex.java
+++ githubtest/src/githubtest11/SoundVoltex.java
@@ -5,7 +5,13 @@ public class SoundVoltex {
 	public static void main(String[] args) {
 		System.out.println("asdfja;lks");
 		System.out.println("qowieur");
-
+		
+		Sdvx A = new Sdvx(); 
+		A.test1();
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println((i % i + 2) + i);
+		}
 	}
 
 }
